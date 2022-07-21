68f3226f8556b986e0d8fc7cc2a291b50ae4ff86
Your Name <you@example.com>
2022-07-21 09:41:04 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+
+const componentPaths: Routes = [
+  {path: 'home', component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
@@ -10,7 +15,7 @@ import { HomeComponent } from './components/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(componentPaths)
   ],
   providers: [],
   bootstrap: [AppComponent]
