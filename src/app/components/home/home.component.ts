d2669b0bab40d7575fc75dc9098e78aa4854bbf4
David Gomez Muns <davidmuns@yahoo.es>
2022-07-22 10:59:03 +0200

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { CalculatorService } from './../../services/calculator.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,30 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  total: number = 0;
-  webPrice: number = 0;
-  seoPrice: number = 0;
-  adsPrice: number = 0;
 
   options = [
     { label: 'Una página web (500€)', price: 500, isChecked: false },
     { label: 'Una consultoria SEO (300€)', price: 300, isChecked: false },
     { label: 'Una campaña de Google Ads (200€)', price: 200, isChecked: false }
   ]
-  constructor() { }
+  constructor(private calculatorService: CalculatorService) { }
 
   ngOnInit(): void { }
 
   public updateTotal() {
+    this.calculatorService.calculateTotal(this.options);
+  }
 
-    this.webPrice = this.options[0].isChecked ? this.options[0].price : 0;
-
-    this.seoPrice = this.options[1].isChecked ? this.options[1].price : 0;
-
-    this.adsPrice = this.options[2].isChecked ? this.options[2].price : 0;
-
-    this.total = this.webPrice + this.seoPrice + this.adsPrice;
-
+  public getTotalWithExtras(): number {
+    return this.calculatorService.getTotalWithExtras();
   }
 
 }
