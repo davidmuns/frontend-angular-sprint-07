06101759fad13749fc8d7e92d67f2181f1278b51
David Gomez Muns <davidmuns@yahoo.es>
2022-07-22 10:59:12 +0200

    Update panell.component.ts

--- src/app/components/panell/panell.component.ts
+++ src/app/components/panell/panell.component.ts
@@ -1,3 +1,4 @@
+import { CalculatorService } from './../../services/calculator.service';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -7,34 +8,16 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PanellComponent implements OnInit {
   @Input('optionsHomeComponent') options: any;
-  @Input('totalHomeComponent') total: number = 0;
 
   pages: number = 0;
   languages: number = 0;
-  totalPlusExtras: number = 0;
 
-  constructor() { }
+  constructor(private calculatorService: CalculatorService) { }
 
   ngOnInit(): void { }
 
-  public addExtrasToWeb() {
-    let extra: number = 0
-    this.pages = this.pages < 0 ? 0 : this.pages;
-    this.languages = this.languages < 0 ? 0 : this.languages;
-    if (this.pages === 0 && this.languages != 0) {
-      extra = this.languages * 30;
-    } else if (this.pages != 0 && this.languages === 0) {
-      extra = this.pages * 30;
-    } else {
-      extra = (this.pages * this.languages) * 30;
-    }
-
-    this.totalPlusExtras = this.total + extra;
-
-  }
-
-  public printTotal() {
-    alert('Total + extras: ' + this.totalPlusExtras)
+  public updateWebExtras() {
+    this.calculatorService.calculateWebExtras(this.pages, this.languages);
   }
 
 }
