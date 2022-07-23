17c58b145e1a5b57082d4c6ba6f4eb87bb65055f
David Gomez Muns <davidmuns@yahoo.es>
2022-07-23 11:40:23 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,12 @@
+import { CalculatorService } from './services/calculator.service';
 import { PanelComponent } from './components/panel/panel.component';
 import { NgModule } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
-
-
-
-const componentPaths: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'home', component: HomeComponent }
-]
 
 @NgModule({
   declarations: [
@@ -25,12 +17,11 @@ const componentPaths: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(componentPaths),
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CalculatorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
