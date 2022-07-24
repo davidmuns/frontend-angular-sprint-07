299c393f01613e40750a5f0f6101c8ad4484e364
David Gomez Muns <davidmuns@yahoo.es>
2022-07-24 19:37:26 +0200

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { CalculatorService } from './../../services/calculator.service';
 import { Component, OnInit } from '@angular/core';
-import { Option } from 'src/app/interfaces/option';
+import { Option } from '../../interfaces/option';
 
 @Component({
   selector: 'app-home',
