c880c98a4c2cc9f4a7460d57b71d2556f2d874e0
David Gomez Muns <davidmuns@yahoo.es>
2022-07-25 22:24:28 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { WebPanelModalComponent } from './modal/web-panel-modal/web-panel-modal.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { WelcomePageComponent } from './components/welcome-page/welcome-page.com
     HomeComponent,
     PanelComponent,
     WelcomePageComponent,
+    WebPanelModalComponent,
   ],
   imports: [
     BrowserModule,
