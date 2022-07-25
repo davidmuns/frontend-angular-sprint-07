cbf280b15cf0c2af292a2c43d7b17b8f2b037cbe
David Gomez Muns <davidmuns@yahoo.es>
2022-07-25 22:24:47 +0200

    Create web-panel-modal.component.ts

---
+++ src/app/modal/web-panel-modal/web-panel-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-web-panel-modal',
+  templateUrl: './web-panel-modal.component.html',
+  styleUrls: ['./web-panel-modal.component.css']
+})
+export class WebPanelModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
