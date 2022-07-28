3e412d4aaaadba95c216dadc822907ecd5df57eb
David Gomez Muns <davidmuns@yahoo.es>
2022-07-28 19:42:49 +0200

    sort functions

--- src/app/components/budget-list/budget-list.component.ts
+++ src/app/components/budget-list/budget-list.component.ts
@@ -15,6 +15,19 @@ export class BudgetListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public sortBudgetListAlphabeticallyByName() {
+    this.budgets = this.budgets
+      .sort((budget1, budget2) => {
+        return budget1.getBudgetName() === budget2.getBudgetName() ? 0 : budget1.getBudgetName() > budget2.getBudgetName() ? 1 : -1;
+      })
+    console.log(this.budgets);
+  }
+
+  public sortBudgetListByDate() {
+    this.budgets = this.budgets.sort((budget1, budget2) => Number(budget1.getDate()) - Number(budget2.getDate()))
+
+  }
+
 
 
 }
--- src/app/models/budget.ts
+++ src/app/models/budget.ts
@@ -1,40 +1,50 @@
 export class Budget {
-    private budgetName: string;
-    private clientName: string;
-    private labels: string[];
-    private webExtras = {
-        pages: 0,
-        languages: 0
-    }
-    private total: string;
+  private id: number = Math.floor(Math.random() * 1000) + 1;
+  private budgetName: string;
+  private clientName: string;
+  private labels: string[];
+  private webExtras = {
+    pages: 0,
+    languages: 0
+  }
+  private total: string;
+  private date: Date = new Date();
 
-    constructor(budgetName: string, clientName: string, labels: string[], total: string) {
-        this.budgetName = budgetName;
-        this.clientName = clientName;
-        this.labels = labels;
-        this.total = total;
-    }
+  constructor(budgetName: string, clientName: string, labels: string[], total: string) {
+    this.budgetName = budgetName;
+    this.clientName = clientName;
+    this.labels = labels;
+    this.total = total;
+  }
 
-    public setWebExtras(pages: number, languages: number) : void {
-        this.webExtras.pages = pages;
-        this.webExtras.languages = languages;
-    }
-    public getBudgetName(): string{
-      return this.budgetName;
-    }
+  public setWebExtras(pages: number, languages: number): void {
+    this.webExtras.pages = pages;
+    this.webExtras.languages = languages;
+  }
+  public getBudgetName(): string {
+    return this.budgetName;
+  }
 
-    public getClientName(): string{
-      return this.clientName;
-    }
-    public getLabels(): string []{
-      return this.labels;
-    }
+  public getClientName(): string {
+    return this.clientName;
+  }
+  public getLabels(): string[] {
+    return this.labels;
+  }
 
-    public getWebExtras(){
-      return this.webExtras;
-    }
+  public getWebExtras() {
+    return this.webExtras;
+  }
 
-    public getTotal(): string {
-      return this.total;
-    }
+  public getTotal(): string {
+    return this.total;
+  }
+
+  public getDate() {
+    return this.date;
+  }
+
+  public getId() {
+    return this.id;
+  }
 }
