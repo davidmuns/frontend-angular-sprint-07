48c41ef2b252e3dd07dc9f385200af09ed95691e
Your Name <you@example.com>
2022-09-12 13:03:46 +0200

    refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { WelcomePageComponent } from './components/welcome-page/welcome-page.com
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BudgetListComponent } from './components/budget-list/budget-list.component';
 
 
 const routes: Routes = [
   { path: '', component: WelcomePageComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'list', component: BudgetListComponent},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
